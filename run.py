a5df878c7488332369c5fa228b0bd6694ef23414
Oliver <lidongyue12138@gmail.com>
2018-12-19 21:43:24 +0800

    Reformat Code

--- run.py
+++ run.py
@@ -1,5 +1,19 @@
-from vggNet import Model
+import numpy as np
+
 from CIFAR10_DataLoader import CifarDataManager, display_cifar
+from vggNet import Model
+
+'''
+Configuration:
+    1. learning rate 
+    2. L1 loss penalty
+    3. Lambda cotrol gate threshold
+'''
+learning_rate = 0.1
+L1_loss_penalty = 0.02
+threshold = 0.1
+''''''
+
 
 d = CifarDataManager()
 print("Number of train images: {}".format(len(d.train.images)))
@@ -11,12 +25,16 @@ images = d.train.images
 print(images.shape)
 
 
-model = Model()
+model = Model(
+    learning_rate = learning_rate, 
+    L1_loss_penalty = L1_loss_penalty,
+    threshold = threshold
+)
 
-model.build_model(100)
-
-model.restore_model()
-
-for i in range(10):
-    generatedGate = model.encode_input(d.train.images[0].reshape((1,32,32,3)))
+'''
+Running Test
+'''
+for i in range(100, 101):
+    generatedGate = model.compute_encoding(d.train.images[i].reshape((1,32,32,3)))
     print(generatedGate)
+
--- vggNet.py
+++ vggNet.py
@@ -1,15 +1,16 @@
+import pickle
+import random
+# from decimal import *
+
+import keras
 import numpy as np
 import tensorflow as tf
-import random
-import keras
-import pickle
 from sklearn.utils import shuffle
-from decimal import *
 
 
 # ================== VGG Network Model with Control Gates ==================
 class Model():
-    def __init__(self):
+    def __init__(self, learning_rate = 0.1, L1_loss_penalty = 0.02, threshold = 0.1):
         '''
         For one input image :
             1. Store all the gates infomation
@@ -18,35 +19,62 @@ class Model():
         self.AllGateVariables = dict()
         self.AllGateVariableValues = list()
 
-        self.sess = tf.Session()
+        '''
+        For encode images super parameters:
+            1. learning rate
+            2. L1 penalty
+            3. Lambda control gate threshold
+        '''
+        self.learning_rate = learning_rate
+        self.L1_loss_penalty = L1_loss_penalty
+        self.threshold = threshold
+
+    '''
+    Wrap Functions:
+        1. model.build_model()
+        2. model.restore_model()
+        3. model.encode_input()
+    '''
+    def compute_encoding(self, data_input):
+        self.AllGateVariableValues.clear()
+        self.AllGateVariables.clear()
+
+        graph = tf.Graph()
+        self.build_model(graph, 100)
+        self.restore_model(graph)
+        generatedGates = self.encode_input(data_input)
+        self.close_sess()
+        return np.array(generatedGates)
 
     '''
     Restore the original network weights
     '''
-    def restore_model(self):
+    def restore_model(self, graph):
         savedVariable = {}
-        
-        self.sess.run(tf.global_variables_initializer())
 
-        for i in tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES):
-            variable = i
-            name = i.name
-            if name == 'pl:0':
-                continue
-            if name in self.AllGateVariables:
-                continue 
-            if len(name) >= 8 and name[-11:] == '/Momentum:0':
-                name_prefix = name[:-11]
-                name_prefix += ':0'
-                if name_prefix in self.AllGateVariables:
+        self.sess = tf.Session(graph = graph)
+        self.sess.run(self.init)
+
+        with graph.as_default():
+            for i in tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES):
+                variable = i
+                name = i.name
+                if name == 'pl:0':
                     continue
-            name = i.name[:-2]
-            savedVariable[name] = variable
-        saver = tf.train.Saver(savedVariable)
-        # saver = tf.train.Saver(max_to_keep = None)
-        saver.restore(self.sess, "vggNet/augmentation.ckpt-120")
-        print("Restored successfully!")
-        
+                if name in self.AllGateVariables:
+                    continue 
+                if len(name) >= 8 and name[-11:] == '/Momentum:0':
+                    name_prefix = name[:-11]
+                    name_prefix += ':0'
+                    if name_prefix in self.AllGateVariables:
+                        continue
+                name = i.name[:-2]
+                savedVariable[name] = variable
+            saver = tf.train.Saver(savedVariable)
+            # saver = tf.train.Saver(max_to_keep = None)
+            saver.restore(self.sess, "vggNet/augmentation.ckpt-120")
+            print("Restored successfully!")
+            
 
         # print(tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES))
         # print(tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES))
@@ -54,89 +82,93 @@ class Model():
     '''
     Build VGG Network with Control Gate Lambdas
     '''
-    def build_model(self, label_count):
-        '''
-        Place Holders:
-            1. input_x: data
-            2. input_y: original predicted labels
-            3. learning rate
-            4. drop keeping probability: no drop layer actually
-            5. whether in training mode: always False
-            6. penalty: regularization
-        '''
-        self.xs = tf.placeholder("float", shape=[None, 32, 32, 3])
-        self.ys_orig = tf.placeholder("float", shape=[None, label_count])
-        self.lr = tf.placeholder("float", shape=[])
-        self.keep_prob = tf.placeholder(tf.float32)
-        self.is_training = tf.placeholder("bool", shape=[])
-        self.penalty = tf.placeholder(tf.float32)
+    def build_model(self, graph, label_count):
+        with graph.as_default():
+            '''
+            Place Holders:
+                1. input_x: data
+                2. input_y: original predicted labels
+                3. learning rate
+                4. drop keeping probability: no drop layer actually
+                5. whether in training mode: always False
+                6. penalty: regularization
+            '''
+            self.xs = tf.placeholder("float", shape=[None, 32, 32, 3])
+            self.ys_orig = tf.placeholder("float", shape=[None, label_count])
+            self.lr = tf.placeholder("float", shape=[])
+            self.keep_prob = tf.placeholder(tf.float32)
+            self.is_training = tf.placeholder("bool", shape=[])
+            self.penalty = tf.placeholder(tf.float32)
 
-        '''
-        VGG Network Model Construction with Control Gates 
-        '''
-        with tf.variable_scope("Conv1", reuse = tf.AUTO_REUSE):
-            current = self.batch_activ_conv(self.xs, 3, 64, 3, self.is_training, self.keep_prob)
-        with tf.variable_scope("Conv2", reuse = tf.AUTO_REUSE):
-            current = self.batch_activ_conv(current, 64, 64, 3, self.is_training, self.keep_prob)
-            current = self.maxpool2d(current, k=2)
-        with tf.variable_scope("Conv3", reuse = tf.AUTO_REUSE):
-            current = self.batch_activ_conv(current, 64, 128, 3, self.is_training, self.keep_prob)
-        with tf.variable_scope("Conv4", reuse = tf.AUTO_REUSE):
-            current = self.batch_activ_conv(current, 128, 128, 3, self.is_training, self.keep_prob)
-            current = self.maxpool2d(current, k=2)
-        with tf.variable_scope("Conv5", reuse = tf.AUTO_REUSE):
-            current = self.batch_activ_conv(current, 128, 256, 3, self.is_training, self.keep_prob)
-        with tf.variable_scope("Conv6", reuse = tf.AUTO_REUSE):
-            current = self.batch_activ_conv(current, 256, 256, 3, self.is_training, self.keep_prob)
-        with tf.variable_scope("Conv7", reuse = tf.AUTO_REUSE):
-            current = self.batch_activ_conv(current, 256, 256, 1, self.is_training, self.keep_prob)
-            current = self.maxpool2d(current, k=2)
-        with tf.variable_scope("Conv8", reuse = tf.AUTO_REUSE):
-            current = self.batch_activ_conv(current, 256, 512, 3, self.is_training, self.keep_prob)
-        with tf.variable_scope("Conv9", reuse = tf.AUTO_REUSE):
-            current = self.batch_activ_conv(current, 512, 512, 3, self.is_training, self.keep_prob)
-        with tf.variable_scope("Conv10", reuse = tf.AUTO_REUSE):
-            current = self.batch_activ_conv(current, 512, 512, 1, self.is_training, self.keep_prob)
-            current = self.maxpool2d(current, k=2)
-        with tf.variable_scope("Conv11", reuse = tf.AUTO_REUSE):
-            current = self.batch_activ_conv(current, 512, 512, 3, self.is_training, self.keep_prob)
-        with tf.variable_scope("Conv12", reuse = tf.AUTO_REUSE):
-            current = self.batch_activ_conv(current, 512, 512, 3, self.is_training, self.keep_prob)
-        with tf.variable_scope("Conv13", reuse = tf.AUTO_REUSE):
-            current = self.batch_activ_conv(current, 512, 512, 1, self.is_training, self.keep_prob)
-            current = self.maxpool2d(current, k=2)
-            current = tf.reshape(current, [ -1, 512 ])
-        with tf.variable_scope("FC14", reuse = tf.AUTO_REUSE):
-            current = self.batch_activ_fc(current, 512, 4096, self.is_training)
-        with tf.variable_scope("FC15", reuse = tf.AUTO_REUSE):
-            current = self.batch_activ_fc(current, 4096, 4096, self.is_training)
-        with tf.variable_scope("FC16", reuse = tf.AUTO_REUSE):
-            Wfc = self.weight_variable_xavier([ 4096, label_count ], name = 'W')
-            bfc = self.bias_variable([ label_count ])
-            self.ys_pred = tf.matmul(current, Wfc) + bfc
+            '''
+            VGG Network Model Construction with Control Gates 
+            '''
+            with tf.variable_scope("Conv1", reuse = tf.AUTO_REUSE):
+                current = self.batch_activ_conv(self.xs, 3, 64, 3, self.is_training, self.keep_prob)
+            with tf.variable_scope("Conv2", reuse = tf.AUTO_REUSE):
+                current = self.batch_activ_conv(current, 64, 64, 3, self.is_training, self.keep_prob)
+                current = self.maxpool2d(current, k=2)
+            with tf.variable_scope("Conv3", reuse = tf.AUTO_REUSE):
+                current = self.batch_activ_conv(current, 64, 128, 3, self.is_training, self.keep_prob)
+            with tf.variable_scope("Conv4", reuse = tf.AUTO_REUSE):
+                current = self.batch_activ_conv(current, 128, 128, 3, self.is_training, self.keep_prob)
+                current = self.maxpool2d(current, k=2)
+            with tf.variable_scope("Conv5", reuse = tf.AUTO_REUSE):
+                current = self.batch_activ_conv(current, 128, 256, 3, self.is_training, self.keep_prob)
+            with tf.variable_scope("Conv6", reuse = tf.AUTO_REUSE):
+                current = self.batch_activ_conv(current, 256, 256, 3, self.is_training, self.keep_prob)
+            with tf.variable_scope("Conv7", reuse = tf.AUTO_REUSE):
+                current = self.batch_activ_conv(current, 256, 256, 1, self.is_training, self.keep_prob)
+                current = self.maxpool2d(current, k=2)
+            with tf.variable_scope("Conv8", reuse = tf.AUTO_REUSE):
+                current = self.batch_activ_conv(current, 256, 512, 3, self.is_training, self.keep_prob)
+            with tf.variable_scope("Conv9", reuse = tf.AUTO_REUSE):
+                current = self.batch_activ_conv(current, 512, 512, 3, self.is_training, self.keep_prob)
+            with tf.variable_scope("Conv10", reuse = tf.AUTO_REUSE):
+                current = self.batch_activ_conv(current, 512, 512, 1, self.is_training, self.keep_prob)
+                current = self.maxpool2d(current, k=2)
+            with tf.variable_scope("Conv11", reuse = tf.AUTO_REUSE):
+                current = self.batch_activ_conv(current, 512, 512, 3, self.is_training, self.keep_prob)
+            with tf.variable_scope("Conv12", reuse = tf.AUTO_REUSE):
+                current = self.batch_activ_conv(current, 512, 512, 3, self.is_training, self.keep_prob)
+            with tf.variable_scope("Conv13", reuse = tf.AUTO_REUSE):
+                current = self.batch_activ_conv(current, 512, 512, 1, self.is_training, self.keep_prob)
+                current = self.maxpool2d(current, k=2)
+                current = tf.reshape(current, [ -1, 512 ])
+            with tf.variable_scope("FC14", reuse = tf.AUTO_REUSE):
+                current = self.batch_activ_fc(current, 512, 4096, self.is_training)
+            with tf.variable_scope("FC15", reuse = tf.AUTO_REUSE):
+                current = self.batch_activ_fc(current, 4096, 4096, self.is_training)
+            with tf.variable_scope("FC16", reuse = tf.AUTO_REUSE):
+                Wfc = self.weight_variable_xavier([ 4096, label_count ], name = 'W')
+                bfc = self.bias_variable([ label_count ])
+                self.ys_pred = tf.matmul(current, Wfc) + bfc
 
-        '''
-        Loss Definition
-        '''
-        # prediction = tf.nn.softmax(ys_)
-        # conv_value = tf.add_n([tf.reduce_sum(tf.abs(w)) for w in convValues])
-        self.cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
-            logits = self.ys_pred, labels = self.ys_orig
-        ))
-        l1_loss = tf.add_n([tf.reduce_sum(tf.abs(w)) for w in self.AllGateVariableValues])
-        self.l1_loss = l1_loss * self.penalty
-        self.total_loss = self.l1_loss + self.cross_entropy
-        
-        '''
-        Optimizer
-        '''
-        self.train_step = tf.train.MomentumOptimizer(self.lr, 0.9, use_nesterov=True).minimize(self.total_loss)
-        
-        '''
-        Check whether correct
-        '''
-        correct_prediction = tf.equal(tf.argmax(self.ys_orig, 1), tf.argmax(self.ys_pred, 1))
-        self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+            self.ys_pred_softmax = tf.nn.softmax(self.ys_pred)
+            '''
+            Loss Definition
+            '''
+            # prediction = tf.nn.softmax(ys_)
+            # conv_value = tf.add_n([tf.reduce_sum(tf.abs(w)) for w in convValues])
+            self.cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
+                logits = self.ys_pred, labels = self.ys_orig
+            ))
+            l1_loss = tf.add_n([tf.reduce_sum(tf.abs(w)) for w in self.AllGateVariableValues])
+            self.l1_loss = l1_loss * self.penalty
+            self.total_loss = self.l1_loss + self.cross_entropy
+            
+            '''
+            Optimizer
+            '''
+            self.train_step = tf.train.MomentumOptimizer(self.lr, 0.9, use_nesterov=True).minimize(self.total_loss)
+            
+            '''
+            Check whether correct
+            '''
+            correct_prediction = tf.equal(tf.argmax(self.ys_orig, 1), tf.argmax(self.ys_pred, 1))
+            self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+
+            self.init = tf.global_variables_initializer()
         
 
     '''
@@ -144,24 +176,25 @@ class Model():
     Produce a lambda code
     '''
     def encode_input(self, input_data):
-        generateGate = []
-        learning_rate = 0.1
-        penalty = 0.05
+        generateGate = np.array([])
+        learning_rate = self.learning_rate
+        L1_loss_penalty = self.L1_loss_penalty
+        threshold = self.threshold
 
     
-        label_orig = self.sess.run(self.ys_pred, feed_dict = {
+        label_orig = self.sess.run(self.ys_pred_softmax, feed_dict = {
             self.xs : input_data, 
             self.lr : learning_rate, 
             self.keep_prob : 1.0, 
             self.is_training : False, 
-            self.penalty : penalty          
+            self.penalty : L1_loss_penalty          
         })
 
         tmpLoss = 1000
         for epoch in range(100):
             if epoch == 50: 
                 learning_rate /= 10
-                penalty *= 10
+                # penalty *= 10
 
             self.sess.run(self.train_step, feed_dict = {
                 self.xs: input_data,
@@ -169,31 +202,33 @@ class Model():
                 self.lr : learning_rate, 
                 self.keep_prob : 1.0, 
                 self.is_training : False, 
-                self.penalty : penalty
+                self.penalty : L1_loss_penalty
             })
             
-            [L1_loss, accuracy] = self.sess.run([self.l1_loss, self.accuracy], feed_dict = {
+            [cross_entropy, L1_loss, accuracy] = self.sess.run([self.cross_entropy, self.l1_loss, self.accuracy], feed_dict = {
                 self.xs: input_data,
                 self.ys_orig : label_orig, 
                 self.lr : learning_rate, 
                 self.keep_prob : 1.0, 
                 self.is_training : False, 
-                self.penalty : penalty
+                self.penalty : L1_loss_penalty
             })
 
-            print("Epoch: {}: L1_loss: {}, Accuracy: {}".format(epoch, L1_loss, accuracy))
+            print("Epoch: {}: Cross_Entropy: {}, L1_loss: {}, Accuracy: {}".format(
+                epoch, cross_entropy, L1_loss, accuracy))
 
             newGate = []
             for gate in self.AllGateVariables.values():
                 tmp = gate.eval(session=self.sess)
-                newGate.append(list(tmp))
+                tmp[tmp < threshold] = 0
+                newGate.append(tmp)
             if L1_loss == 'nan' or L1_loss > tmpLoss:
                 continue
             if accuracy > 0.99 and L1_loss != 'nan' and L1_loss < 1000:
                 generateGate = np.array(newGate)
                 tmpLoss = L1_loss
         
-        return generateGate
+        return np.array(generateGate)
 
 
     '''
