f2dbaaeb5491624bf67d3d066facbec683951f2d
rogercmq <34480513+rogercmq@users.noreply.github.com>
2018-12-23 23:49:59 +0800

    Revert "xxx"
    This reverts commit 41d34eeaf80fdb7586aa7ab7524e903d9956005b.

--- test.py
+++
@@ -1,9 +0,0 @@
-from vggTrimmedModel import TrimmedModel
-from CIFAR_DataLoader import CifarDataManager
-import numpy as np
-
-d = CifarDataManager()
-model = TrimmedModel(target_class_id = [0,1,2,3], multiPruning = True)
-model.assign_weight()
-test_images, test_labels = d.test.next_batch(200)
-model.test_accuracy(test_images, test_labels)
